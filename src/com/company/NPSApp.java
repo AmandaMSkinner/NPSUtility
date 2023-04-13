5ea5e6a5f3d98257ff0cf3fc143e1fff0a2706d2
Amanda Skinner <amandamuscha@yahoo.com>
2023-04-12 23:14:52 -0400

    added basic query

--- src/com/company/NPSApp.java
+++ src/com/company/NPSApp.java
@@ -3,11 +3,20 @@ package com.company;
 import com.company.model.Park;
 import com.company.services.ParkService;
 
+import java.util.Scanner;
+
 public class NPSApp {
 
     public static void main(String[] args) {
 	// write your code here
         ParkService parkService = new ParkService();
-        System.out.println(parkService.getPark("yose"));
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Enter a 4-letter park code");
+        String pc = scan.nextLine();
+        while(pc.length()!=4){
+            System.out.println("Invalid: Enter a park code");
+            pc = scan.nextLine();
+        }
+        System.out.println(parkService.getPark(pc));
     }
 }
